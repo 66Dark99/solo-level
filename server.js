f2db61cbf9b7f55b4e67e216360032461da48850
66Dark99 <kareem.mohammed.me@gmail.com>
2025-04-19 18:55:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const { Pool } = require('pg');
-const bcrypt = require('bcryptjs'); // استخدام bcryptjs بدلاً من bcrypt
+const bcrypt = require('bcryptjs'); // استخدام bcryptjs
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
